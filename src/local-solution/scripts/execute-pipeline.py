d0394014a2b143eb18f2c1eb437c439f330cb30c
Matt Matchett <matt.matchett23@gmail.com>
2024-08-11 11:03:23 +0200

    chore: add pipeline feedback

--- src/local-solution/scripts/execute-pipeline.py
+++ src/local-solution/scripts/execute-pipeline.py
@@ -16,18 +16,42 @@ import datareporting.exportreporting as exportreporting
 import datareporting.buildreports as buildreports
 
 # Land Raw Data Steps
+print("\n##### PIPELINE STARTING #####\n")
+
+print("##### LANDING PHASE #####")
+print("Importing Data to RAW Directory")
 landrawdata.main()
+print("Import Successful")
+print("Loading Raw data into DB")
 dbloadraw.main()
+print("Load Successful")
+print("##### LANDING COMPLETE #####\n")
 
 # Clean Data Steps
+print("##### CLEANING PHASE #####")
+print("Cleaning Auctions")
 cleanauctions.main()
+print("Cleaning Bids")
 cleanbids.main()
+print("Cleaning Buyers")
 cleanbuyers.main()
+print("Loading Clean data into DB")
 dbloadclean.main()
+print("Load Successful")
+print("##### CLEANING COMPLETE #####\n")
 
 # Reporting Data Steps
+print("##### REPORTING PHASE #####")
+print("Converting data to star schema")
 convertschema.main()
+print("Exporting reporting tables to REPORTING Directory")
 exportreporting.main()
+print("##### REPORTING COMPLETE #####\n")
 
 # Visualisation Data Steps
+print("##### VISUALIZATION PHASE #####")
+print("Building & exporting visuals")
 buildreports.main()
+print("##### VISUALIZATION COMPLETE #####\n")
+
+print("##### PIPELINE COMPLETE#####\n")
